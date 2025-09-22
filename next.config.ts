fe52544c82aadbd820c39227d751ab37fe7a839a
Mohamed Moughamir <moughamir@gmail.com>
2025-09-22 21:53:44 +0100

    update cor

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  async headers() {
+        return [
+          {
+            source: "/api/:path*", 
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "https://two-meskinz.vercel.app" },
+
+              { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ],
+          },
+        ];
+      },
   images: {
     remotePatterns: [
       {
